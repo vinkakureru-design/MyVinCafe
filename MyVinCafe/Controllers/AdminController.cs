46ca056816e3eb3c477a42d18ac53a7ab185d88e
Nadeshiko Chan <vinkakureru@gmail.com>
2026-01-19 21:38:39 +0700

    tambah sedikit fitur di _Layout dan membuat jalan ke index admin dengan controller

---
+++ MyVinCafe/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MyVinCafe.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Tactical Command Center";
+            return View();
+        }
+    }
+}
